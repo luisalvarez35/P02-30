1c03acc3fc14eff8abcebaea9892303ecabc0888
luisalvarez35 <luis.alvarez35@educa.madrid.org>
2020-11-10 22:45:31 +0100

    Problema 4

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Prueba");
-		SSystem.out.println("Prueba4 en los dos");
+		SSystem.out.println("Prueba4 en los dos 2");
 
     }
 }
