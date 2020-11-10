b723763588bbedc9b7b7d92dfb75cac89e1f2f0f
luisalvarez35 <luis.alvarez35@educa.madrid.org>
2020-11-10 22:46:43 +0100

    Problema 4.2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 <<<<<<< HEAD
 		System.out.println("Prueba4 en los dos");
 =======
-		SSystem.out.println("Prueba4 en los dos");
+		SSystem.out.println("Prueba4 en los dos 1");
 >>>>>>> 3d933c63a89e98c282be4c6eff09a7a365f2535c
 
     }
