f562bfef42f348f1c4e4666c16427ab3df8db007
luisalvarez35 <luis.alvarez35@educa.madrid.org>
2020-11-10 22:42:43 +0100

    Prueba4

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Prueba");
-		System.out.println("Prueba1");
+		System.out.println("Prueba4 en los dos");
 
     }
 }
